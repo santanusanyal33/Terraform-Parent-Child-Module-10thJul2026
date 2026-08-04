bf01deebd5c24bc7ca3c61eddbf36e7a1c13e995
santanusanyal33 <santanu.sanyal33@gmail.com>
2026-08-04 23:06:24 +0530

    practice code

---
+++ child/azurerm_vnet/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_virtual_network" "santaVnet"{
+    for_each = var.vnet
+        name = each.value.virtual_network_name
+        location = each.value.location
+        resource_group_name = each.value.resource_group_name
+        address_space = each.value.address_space
+    }
